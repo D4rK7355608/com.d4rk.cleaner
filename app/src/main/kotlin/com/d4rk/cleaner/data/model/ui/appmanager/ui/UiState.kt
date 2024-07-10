7a37011f539d79fe7047f950a815bd00a5ec2a78
D4rK7355608 <d4rk7355608@gmail.com>
2024-07-10 21:23:48 +0300

    Code cleanup

--- app/src/main/kotlin/com/d4rk/cleaner/data/model/ui/appmanager/ui/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.d4rk.cleaner.data.model.ui.appmanager.ui
-
-sealed class UiState<out T : Any> {
-    object Loading : UiState<Nothing>()
-    data class Success<out T : Any>(val data: T) : UiState<T>()
-    data class Error(val exception: Exception) : UiState<Nothing>()
-}
