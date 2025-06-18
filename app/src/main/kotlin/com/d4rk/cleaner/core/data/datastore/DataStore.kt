a3231249270aed50ef53bc68797efb9427233cec
D4rK <d4rk7355608@gmail.com>
2025-06-18 21:40:57 +0300

    Redesign storage permission onboarding

--- app/src/main/kotlin/com/d4rk/cleaner/core/data/datastore/DataStore.kt
+++ app/src/main/kotlin/com/d4rk/cleaner/core/data/datastore/DataStore.kt
@@ -295,4 +295,26 @@ class DataStore(val context : Context) : CommonDataStore(context = context) {
             prefs[usagePermissionGrantedKey] = granted
         }
     }
+
+    private val documentTreePermissionGrantedKey = booleanPreferencesKey("permission_document_tree_granted")
+    val documentTreePermissionGranted: Flow<Boolean> = dataStore.data.map { prefs ->
+        prefs[documentTreePermissionGrantedKey] == true
+    }
+
+    private val documentTreeUriKey = stringPreferencesKey("document_tree_uri")
+    val documentTreeUri: Flow<String?> = dataStore.data.map { prefs ->
+        prefs[documentTreeUriKey]
+    }
+
+    suspend fun saveDocumentTreePermissionGranted(granted: Boolean) {
+        dataStore.edit { prefs ->
+            prefs[documentTreePermissionGrantedKey] = granted
+        }
+    }
+
+    suspend fun saveDocumentTreeUri(uri: String) {
+        dataStore.edit { prefs ->
+            prefs[documentTreeUriKey] = uri
+        }
+    }
 }
