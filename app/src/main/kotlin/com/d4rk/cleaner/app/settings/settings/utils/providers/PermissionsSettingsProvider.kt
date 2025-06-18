07e3143ce2867b215de7a29682f373061cc9fe4c
D4rK7355608 <d4rk7355608@gmail.com>
2025-06-18 21:16:56 +0300

    Refactor: Reorganize permissions in settings
    This commit reorganizes the permissions listed in the settings screen into more logical categories: Normal, Runtime, Special, and Storage. This improves the clarity and usability of the permissions settings.

--- app/src/main/kotlin/com/d4rk/cleaner/app/settings/settings/utils/providers/PermissionsSettingsProvider.kt
+++ app/src/main/kotlin/com/d4rk/cleaner/app/settings/settings/utils/providers/PermissionsSettingsProvider.kt
@@ -8,97 +8,106 @@ import com.d4rk.android.libs.apptoolkit.app.settings.settings.domain.model.Setti
 import com.d4rk.cleaner.R
 
 class PermissionsSettingsProvider : PermissionsProvider {
-    override fun providePermissionsConfig(context : Context) : SettingsConfig {
+    override fun providePermissionsConfig(context: Context): SettingsConfig {
         return SettingsConfig(
-            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.permissions) , categories = listOf(
+            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.permissions),
+            categories = listOf(
                 SettingsCategory(
-                    title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.normal) ,
+                    title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.normal),
                     preferences = listOf(
                         SettingsPreference(
-                            title = context.getString(R.string.ad_id) ,
-                            summary = context.getString(R.string.summary_preference_permissions_ad_id) ,
-                        ) ,
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.access_network_state),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_access_network_state),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.internet) ,
-                            summary = context.getString(R.string.summary_preference_permissions_internet) ,
-                        ) ,
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.ad_id),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_ad_id),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.post_notifications) ,
-                            summary = context.getString(R.string.summary_preference_permissions_post_notifications) ,
-                        ) ,
-                    ) ,
-                ) , SettingsCategory(
-                    title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.normal) ,
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.billing),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_billing),
+                        ),
+                        SettingsPreference(
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.check_license),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_check_license),
+                        ),
+                        SettingsPreference(
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.foreground_service),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_foreground_service),
+                        ),
+                        SettingsPreference(
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.internet),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_internet),
+                        ),
+                        SettingsPreference(
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.wake_lock),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_wake_lock),
+                        ),
+                    ),
+                ), SettingsCategory(
+                    title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.runtime),
                     preferences = listOf(
                         SettingsPreference(
-                            title = context.getString(R.string.access_network_state) ,
-                            summary = context.getString(R.string.summary_preference_permissions_access_network_state) ,
-                        ) ,
-                        SettingsPreference(
-                            title = context.getString(R.string.access_notification_policy) ,
-                            summary = context.getString(R.string.summary_preference_permissions_access_notification_policy) ,
-                        ) ,
-                        SettingsPreference(
-                            title = context.getString(R.string.billing) ,
-                            summary = context.getString(R.string.summary_preference_permissions_billing) ,
-                        ) ,
-                        SettingsPreference(
-                            title = context.getString(R.string.check_license) ,
-                            summary = context.getString(R.string.summary_preference_permissions_check_license) ,
-                        ) ,
-                        SettingsPreference(
-                            title = context.getString(R.string.foreground_service) ,
-                            summary = context.getString(R.string.summary_preference_permissions_foreground_service) ,
-                        ) ,
-                        SettingsPreference(
-                            title = context.getString(R.string.request_delete_packages) ,
-                            summary = context.getString(R.string.summary_preference_permissions_request_delete_packages) ,
-                        ) ,
-                    ) ,
-                ) , SettingsCategory(
-                    title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.storage) ,
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.post_notifications),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_post_notifications),
+                        ),
+                    ),
+                ), SettingsCategory(
+                    title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.special),
                     preferences = listOf(
                         SettingsPreference(
-                            title = context.getString(R.string.access_media_location) ,
-                            summary = context.getString(R.string.summary_preference_permissions_access_media_location) ,
-                        ) ,
+                            title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.access_notification_policy),
+                            summary = context.getString(com.d4rk.android.libs.apptoolkit.R.string.summary_preference_permissions_access_notification_policy),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.action_open_document_tree) ,
-                            summary = context.getString(R.string.summary_preference_permissions_action_open_document_tree) ,
-                        ) ,
+                            title = context.getString(R.string.request_delete_packages),
+                            summary = context.getString(R.string.summary_preference_permissions_request_delete_packages),
+                        ),
+                    ),
+                ), SettingsCategory(
+                    title = context.getString(com.d4rk.android.libs.apptoolkit.R.string.storage),
+                    preferences = listOf(
                         SettingsPreference(
-                            title = context.getString(R.string.manage_external_storage) ,
-                            summary = context.getString(R.string.summary_preference_permissions_manage_external_storage) ,
-                        ) ,
+                            title = context.getString(R.string.access_media_location),
+                            summary = context.getString(R.string.summary_preference_permissions_access_media_location),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.package_usage_stats) ,
-                            summary = context.getString(R.string.summary_preference_permissions_package_usage_stats) ,
-                        ) ,
+                            title = context.getString(R.string.action_open_document_tree),
+                            summary = context.getString(R.string.summary_preference_permissions_action_open_document_tree),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.query_all_packages) ,
-                            summary = context.getString(R.string.summary_preference_permissions_query_all_packages) ,
-                        ) ,
+                            title = context.getString(R.string.manage_external_storage),
+                            summary = context.getString(R.string.summary_preference_permissions_manage_external_storage),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.read_external_storage) ,
-                            summary = context.getString(R.string.summary_preference_permissions_read_external_storage) ,
-                        ) ,
+                            title = context.getString(R.string.package_usage_stats),
+                            summary = context.getString(R.string.summary_preference_permissions_package_usage_stats),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.read_media_audio) ,
-                            summary = context.getString(R.string.summary_preference_permissions_read_media_audio) ,
-                        ) ,
+                            title = context.getString(R.string.query_all_packages),
+                            summary = context.getString(R.string.summary_preference_permissions_query_all_packages),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.read_media_images) ,
-                            summary = context.getString(R.string.summary_preference_permissions_read_media_images) ,
-                        ) ,
+                            title = context.getString(R.string.read_external_storage),
+                            summary = context.getString(R.string.summary_preference_permissions_read_external_storage),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.read_media_video) ,
-                            summary = context.getString(R.string.summary_preference_permissions_read_media_video) ,
-                        ) ,
+                            title = context.getString(R.string.read_media_audio),
+                            summary = context.getString(R.string.summary_preference_permissions_read_media_audio),
+                        ),
                         SettingsPreference(
-                            title = context.getString(R.string.write_external_storage) ,
-                            summary = context.getString(R.string.summary_preference_permissions_write_external_storage) ,
-                        ) ,
-                    ) ,
+                            title = context.getString(R.string.read_media_images),
+                            summary = context.getString(R.string.summary_preference_permissions_read_media_images),
+                        ),
+                        SettingsPreference(
+                            title = context.getString(R.string.read_media_video),
+                            summary = context.getString(R.string.summary_preference_permissions_read_media_video),
+                        ),
+                        SettingsPreference(
+                            title = context.getString(R.string.write_external_storage),
+                            summary = context.getString(R.string.summary_preference_permissions_write_external_storage),
+                        ),
+                    ),
                 )
             )
         )
