6bd4bf07bd8be67800fc5c5960394255b3bc38cf
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-07-14 23:08:29 +0300

    Fix const DAY_MS to be compile-time constant

--- app/src/main/kotlin/com/d4rk/cleaner/core/utils/constants/TimeConstants.kt
+++ app/src/main/kotlin/com/d4rk/cleaner/core/utils/constants/TimeConstants.kt
@@ -1,7 +1,5 @@
 package com.d4rk.cleaner.core.utils.constants
 
-import java.util.concurrent.TimeUnit
-
 object TimeConstants {
-    const val DAY_MS: Long = TimeUnit.DAYS.toMillis(1)
+    const val DAY_MS: Long = 24L * 60L * 60L * 1000L
 }
