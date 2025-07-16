c4b46beae44ac2bc3aaa1a36f17a0500324d5155
D4rK7355608 <d4rk7355608@gmail.com>
2025-07-16 22:41:15 +0300

    Refactor localeFilters in app/build.gradle.kts
    This commit refactors the `localeFilters` declaration in the `app/build.gradle.kts` file for better readability. The `@Suppress("UnstableApiUsage")` annotation is now on a separate line, and the list of locales is indented.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,8 @@ android {
         versionCode = 202
         versionName = "3.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        @Suppress("UnstableApiUsage") androidResources.localeFilters += listOf(
+        @Suppress("UnstableApiUsage")
+        androidResources.localeFilters += listOf(
             "ar-rEG" , "bg-rBG" , "bn-rBD" , "de-rDE" , "en" , "es-rGQ" , "es-rMX" , "fil-rPH" , "fr-rFR" , "hi-rIN" , "hu-rHU" , "in-rID" , "it-rIT" , "ja-rJP" , "ko-rKR" , "pl-rPL" , "pt-rBR" , "ro-rRO" , "ru-rRU" , "sv-rSE" , "th-rTH" , "tr-rTR" , "uk-rUA" , "ur-rPK" , "vi-rVN" , "zh-rTW"
         )
         vectorDrawables {
