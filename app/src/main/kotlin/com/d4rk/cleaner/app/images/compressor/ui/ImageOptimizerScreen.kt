4a59d7ce0a6d939bb7fefccf1f08d13b2974d887
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-07-18 09:46:10 +0300

    Add missing newline

--- app/src/main/kotlin/com/d4rk/cleaner/app/images/compressor/ui/ImageOptimizerScreen.kt
+++ app/src/main/kotlin/com/d4rk/cleaner/app/images/compressor/ui/ImageOptimizerScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
